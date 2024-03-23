b38f24f7ad1dc02e974b69faaac5ee1d65698ecd
Sergio Gurillo <sergio.gurillo11@gmail.com>
2024-03-23 18:47:05 +0100

    fix class

--- src/main/java/org/jvim/domain/Editor/Editor.java
+++ src/main/java/org/jvim/domain/Editor/Editor.java
@@ -1,11 +1,7 @@
 package org.jvim.domain.Editor;
 
-import org.jvim.domain.Terminal.UnixTerminal;
-
 public class Editor {
     public static int rows = 10;
     public static int columns = 10;
-    public static final Terminal terminal = new UnixTerminal();
-
     public static String statusMessage;
 }
